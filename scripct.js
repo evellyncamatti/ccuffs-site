823b62cf4005d253b1f0107edce477cf02afe00f
evellyncamatti <evellyncco@gmail.com>
2022-08-21 13:53:20 -0300

    Atualização
    Atualizando o site com os conhecimentos adquiridos na matéria Programação II

---
+++ scripct.js
@@ -0,0 +1,7 @@
+function alterarImagem(objeto, caminhoNovaImagem){
+		document.getElementById(objeto).src = caminhoNovaImagem; 
+        /*document.getElementById: acessa um objeto na página através do ID
+		src: é abreviação de source, é o caminho da nova imagem.
+		= caminhoNovaImagem: Informa o diretório da nova imagem
+		*/
+}		
